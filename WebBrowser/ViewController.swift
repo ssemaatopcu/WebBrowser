7fd851901aa14f6046442b755aadf69427dba771
Sema Topcu <ssemaatopcu.@gmail.com>
2024-02-11 16:39:50 +0300

    Initial Commit

---
+++ WebBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebBrowser
+//
+//  Created by Sema Topcu on 2/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
